9be95789cc54953ba07569d7c82913f5e5c75731
OIC-2024-1B07SN-Hayata <C4220@oic.jp>
2024-11-07 12:08:27 +0900

    プロジェクト ファイルを追加します。

---
+++ データ構造及びアルゴリズム/Program.cs
@@ -0,0 +1,17 @@
+﻿char[] strings = { 'A', 'B', 'B', 'B', 'B', 'A', 'A', 'B', 'A', 'A', 'A', 'A', 'A', 
+    'B', 'B', 'B', 'B', 'A', 'A', '#' };
+int i, j, cnt;
+i = 1;
+j = 1;
+cnt = 1;
+do
+{
+    j = j + 1;
+    if(cnt>=4)
+    {
+        strings[i] = strings[j-1];
+        strings[i + 1] = '*';
+        strings[i + 2] = Convert.ToChar(cnt);
+        i = i + 3;
+    }
+}
